ec0b96f51024933ed3b604a33a05aeef6d629e05
quochao <hao.bui@herond.org>
2025-09-22 09:50:22 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const Sentry = require("@sentry/node");
+
+function initSentry({
+  dsn,
+  env = process.env.NODE_ENV,
+  release,
+  extraOptions = {},
+}) {
+  Sentry.init({
+    dsn,
+    environment: env,
+    release,
+    ...extraOptions,
+  });
+
+  // --- Capture unhandled errors automatically ---
+  process.on("uncaughtException", (err) => {
+    Sentry.captureException(err);
+    Sentry.flush(2000).finally(() => process.exit(1));
+  });
+
+  process.on("unhandledRejection", (reason) => {
+    Sentry.captureException(reason);
+    Sentry.flush(2000).finally(() => process.exit(1));
+  });
+
+  return Sentry;
+}
+
+function captureError(err, context = {}) {
+  Sentry.withScope((scope) => {
+    Object.entries(context).forEach(([k, v]) => scope.setExtra(k, v));
+    Sentry.captureException(err);
+  });
+}
+
+module.exports = { initSentry, captureError };
